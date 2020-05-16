7aa631eb6dc6b7b0543439face6c682c11317e21
sobolm01 <Michal.Sobolewski@stepstone.com>
2020-05-16 21:52:37 +0200

    Initial commit

---
+++ src/main/java/com/msobdev/rqueuetaskexecutor/RqueueTaskExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.msobdev.rqueuetaskexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqueueTaskExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RqueueTaskExecutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msobdev/rqueuetaskexecutor/RqueueTaskExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msobdev.rqueuetaskexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RqueueTaskExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
